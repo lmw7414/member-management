548c81cedfaf5ac9e19a72101acda2a010b27667
Lee Min Woo <lmw7414@naver.com>
2025-11-17 00:54:45 +0900

    feat: MemberEntity setter 제거 및 컬럼 제약 명시
    외부에서 생성자 대신 정적 팩토리 메서드를 사용해서 객체를 생성할 수 있도록 변경

--- src/main/java/com/example/membermanagement/member/model/MemberEntity.java
+++ src/main/java/com/example/membermanagement/member/model/MemberEntity.java
@@ -2,22 +2,30 @@ package com.example.membermanagement.member.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@Setter
+@NoArgsConstructor
 @Table(name = "member")
 public class MemberEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(nullable = false, length = 50, unique = true)
     private String username;
+
+    @Column(nullable = false, length = 100)
     private String password;
+
+    @Column(nullable = false, length = 100, unique = true)
     private String email;
+
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @PrePersist
@@ -25,11 +33,13 @@ public class MemberEntity {
         this.createdAt = LocalDateTime.now();
     }
 
+    private MemberEntity(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
     public static MemberEntity of(String username, String password, String email) {
-        MemberEntity memberEntity = new MemberEntity();
-        memberEntity.setUsername(username);
-        memberEntity.setPassword(password);
-        memberEntity.setEmail(email);
-        return memberEntity;
+        return new MemberEntity(username, password, email);
     }
 }
