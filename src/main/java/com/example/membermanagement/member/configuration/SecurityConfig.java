120be4164e4bc3580a97b49bbbbf415bc87a4dc3
Lee Min Woo <lmw7414@naver.com>
2025-11-17 00:02:34 +0900

    feat: 기본 로그인 페이지 제거
    f87d4c9f91ecc2d84334a32cc99a04e91e469e19
    위 커밋에서 설계했을 때 사용자 비밀번호 암호화를 위해 스프링 시큐리티를 추가했었다. 해당 디펜던시에는 기본 로그인 페이지가 존재해 해당 페이지가 뜨지 않도록 설정 추가하였음

--- src/main/java/com/example/membermanagement/member/configuration/SecurityConfig.java
+++ src/main/java/com/example/membermanagement/member/configuration/SecurityConfig.java
@@ -2,7 +2,10 @@ package com.example.membermanagement.member.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -10,5 +13,10 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable).build();
     }
 }
